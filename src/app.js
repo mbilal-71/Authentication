ff13414f35f15b8974896844ec8691b475151e46
Muhammad Bilal <m.bilalferoz1@gmail.com>
2026-09-17 16:40:10 +0500

    auth

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const authRoutes = require('./routes/auth.routes');
 const cookieParser = require('cookie-parser');
+const postRoutes = require('./routes/post.routes');
 
 const app = express();
 
@@ -8,4 +9,5 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/post', postRoutes);
 module.exports = app
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,4 +1,21 @@
 const express = require('express')
 const router = express.Router()
+const jwt = require('jsonwebtoken')
 
-router.post('/register')
+router.post('/create', (req, res) => {
+   
+ const token = req.cookies.token
+ if(!token) {
+    return res.status(401).json({message: 'Unauthorized'})
+ }
+
+try {
+ const decodedToken = jwt.verify(token, process.env.JWT_SECRET)
+res.json({message: 'Post created successfully'})
+} catch (error) {
+    console.log(error)
+    return res.status(401).json({message: 'token invalid'})
+}
+})
+
+module.exports = router
